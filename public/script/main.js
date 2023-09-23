e63b23a0fc77c69fa23d9c16f4d0f40febc10050
Vinícius Gabriel <viniciusggabrielpl@gmail.com>
2023-09-23 02:37:06 -0300

    Editando comentarios

--- public/script/main.js
+++ public/script/main.js
@@ -1,21 +1,21 @@
 // DARK MODE E LIGHT MODE DO SITE 
 
-// Essa parte do código pega o checkbox do input e usa como uma forma de retirar o atributo do html "data-bs-theme" ====
-// Pega o atributo e o click do checkbox e adiciona um evento + função =================================================
-document.getElementById('checkbox').addEventListener('change', function() {
-// Serve para poder usar o checkbox e ver se ele está marcado ou não sem ter que usar o "document.getElementById" ======
+// Essa parte do código pega o checkbox do input e usa como uma forma de retirar o atributo do html "data-bs-theme" ========
+// Pega o atributo e o click do checkbox e adiciona um evento + função =====================================================
+document.getElementById('checkbox').addEventListener('change', function () {
+    // Serve para poder usar o checkbox e ver se ele está marcado ou não sem ter que usar o "document.getElementById" ======
     var checkbox = this;
-// Essas variáveis a baixo pega o id da tag "html" para poder excluir e adicionar os atributos ===========================
+    // Essas variáveis a baixo pega o id da tag "html" para poder excluir e adicionar os atributos =========================
     var modeTheme = document.getElementById('mainTheme');
     var elemento = document.getElementById('buttonTheme')
 
-// Verifica se o checkbox está marcado =================================================================================
+    // Verifica se o checkbox está marcado =================================================================================
     if (checkbox.checked) {
-// Remove o atributo e pode ser vários tipo até imagens ================================================================
+        // Remove o atributo e pode ser vários tipo até imagens ============================================================
         modeTheme.removeAttribute('data-bs-theme');
         elemento.classList.remove('toggleDark');
     } else {
-// Se o checkbox estiver desmarcado, adiciona o atributo novamente =====================================================
+        // Se o checkbox estiver desmarcado, adiciona o atributo novamente =================================================
         modeTheme.setAttribute('data-bs-theme', 'dark');
         elemento.classList.add('toggleDark');
     }
