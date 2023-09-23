9c0d5f981dbb714dc0e1e787202168ca7906da26
Vinícius Gabriel <viniciusggabrielpl@gmail.com>
2023-09-23 18:15:13 -0300

    Barra de pesquisa
    Feito barra de pesquisa com interação com um API e modelagem de filmes

--- public/script/api.js
+++ public/script/api.js
@@ -1,23 +1,49 @@
+// Constantes do site que tem a chave da API e que armazena o que foi inserido no formulário de pesquisa ==
 const apiKey = '6e588772';
-const formularioPesquisa = document.getElementById("search");
-
-// A primeira linda de código define um evento  de submissão com o "onsubmit" e atribui a "ev"
-formularioPesquisa.onsubmit = (ev) => {
-    // Essa parte limita o site a não recarregar quando o formulário for enviado
+const formulaPesquisa = document.getElementById("search");
+// adiciona um evento "ev" a const do formulário ==========================================================
+formulaPesquisa.onsubmit = (ev) => {
     ev.preventDefault();
-    // Pega o valor inserido dentro do campo "pesquisa" que está no HTML 
-    const valorPesquisa = ev.target.pesquisa.value.trim(); //.trim() remove os espaços extras
-    // Mostra se o campo de pesquisa está preenchido se não estiver mostra o erro e retorna
+    // Armazena o que foi escrito na barra de pesquisa e retorna uma msg se nada foi achado ===============
+    const valorPesquisa = ev.target.pesquisa.value.trim();
     if (valorPesquisa == "") {
         alert('Preencha o campo!');
         return;
     }
-    //Essa primeira parte faz a solicitação para o provedor da API
-    fetch(`http://www.omdbapi.com/?apikey=${apiKey}&t=${valorPesquisa}`)
-        // Converte o resultado em json 
-        // A segunda linha mostra o resultado no console do inspecionar
-        // Terceira linha trata possíveis erros 
+
+    fetch(`http://www.omdbapi.com/?apikey=${apiKey}&s=${valorPesquisa}`)
         .then(result => result.json())
-        .then(json => console.log(json))
+        .then(json => carregaLista(json))
         .catch(error => console.error('Erro:', error));
+}
+
+const carregaLista = (json) => {
+    const lista = document.querySelector("div#list")
+    lista.innerHTML = "";
+
+    if (json.Error) {
+        console.error('Erro na resposta da API:', json.Error);
+        return;
+    }
+
+    // Verifique se a resposta da API possui resultados ===================================================
+    if (json.Search && json.Search.length > 0) {
+        json.Search.forEach(movie => {
+            let item = document.createElement("div");
+            item.classList.add("col-md-4", "mb-4", "w-25");
+            // Coloca os atributos dentro do HTML =========================================================
+            item.innerHTML = `<div class="card" style="width: 18rem; height: 700px;">
+                                <img src="${movie.Poster}" class="card-img-top img-fluid" id="card-image" style="height: 70%; object-fit: cover;" alt="">
+                                <div class="card-body">
+                                    <h5 class="card-title">${movie.Title}</h5>
+                                    <p class="card-text">Diretor: ${movie.Director}</p>
+                                    <p class="card-text">Ano: ${movie.Year}</p>
+                                    <p class="card-text">Tipo: ${movie.Type}</p>
+                                </div>
+                             </div>`;
+            lista.appendChild(item);
+        });
+    } else {
+        console.log('Nenhum resultado encontrado.');
+    }
 }
